c079f58248d6f31e248371acfd88734260f04ffa
ABoyNameOrange <67572824+hankshyu@users.noreply.github.com>
2021-04-26 22:11:52 +0800

    login log out page created

---
+++ signup-check.php
@@ -0,0 +1,84 @@
+<?php 
+session_start(); 
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])
+    && isset($_POST['phone']) && isset($_POST['re_password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+	$re_pass = validate($_POST['re_password']);
+	$phone = validate($_POST['phone']);
+
+	if (empty($uname)) {
+		header("Location: signup.php?error=noun");
+		//error=User Name is required
+	    exit();
+	}else if(empty($pass)){
+        header("Location: signup.php?error=nopw");
+		//error=Password is required
+	    exit();
+	}
+	else if(!preg_match("/^([0-9A-Za-z]+)$/",$pass)){
+		header("Location: signup.php?error=pwinvalid");
+		//error=Password is required
+	    exit();
+
+	}
+	else if(empty($re_pass)){
+        header("Location: signup.php?error=norepw");
+		//error=Re Password is required
+	    exit();
+	}
+
+	else if(empty($phone)){
+        header("Location: signup.php?error=noph");
+		//error=Phone Number is required
+	    exit();
+	}
+
+	else if($pass !== $re_pass){
+        header("Location: signup.php?error=pwrepw");
+		//error=The confirmation password  does not match
+	    exit();
+	}
+
+	else{
+
+		// hashing the password
+		$salt=mt_rand(1000,9999);
+
+        $pass =hash('sha256',$pass.strval($salt));
+		$id=mt_rand(10000000000,99999999999);
+
+	    $sql = "select * from User where user_name='$uname'";
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			header("Location: signup.php?error=pwoc");
+			//error=The username is taken try another
+	        exit();
+		}else {
+           $sql2 = "insert into User value('$id','$uname','$salt', '$pass', '$phone')";
+           $result2 = mysqli_query($conn, $sql2);
+           if ($result2) {
+           	 header("Location: login.php?success=Your account has been created successfully");
+	         exit();
+           }else {
+	           	header("Location: signup.php?error=unknown error occurred");
+		        exit();
+           }
+		}
+	}
+	
+}else{
+	header("Location: signup.php");
+	exit();
+}
---
+++ signup.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+     
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<header>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">masksystem</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="login.php">Login</a></li>
+      <li><a href="signup.php">signup</a></li>
+    </ul>
+  </div>
+</nav>
+</header>
+<body>
+
+     <form action="signup-check.php" method="post">
+     	<h2>SIGN UP</h2>
+          <?php 
+               $er1="";
+               $er2="";
+               $er3="";
+               $er4="";
+               ?>
+          
+
+     	<?php if (isset($_GET['error'])) { 
+
+               switch($_GET['error']){
+                    case"noun":
+                         $er1="User Name is required";
+                         break;
+                    case"nopw":
+                         $er3="Password is required";
+                         break;
+                    case"pwinvalid":
+                         $er3="Invalid Format(Only letters and numbers are available)";
+                         break;
+                    case"norepw":
+                         $er4="Please confirm the password";
+                         break;
+                    case"noph":
+                         $er2="Phone Number is required";
+                         break;
+                    case"pwrepw":
+                         $er3="The password does not match";
+                         $er4="The password does not match";
+                         break;
+                    case"pwoc":
+                         $er1="The username is taken, please try another one =)";
+                         break;
+
+               }
+
+
+     	 } ?>
+          
+          <?php if (isset($_GET['error'])) { ?>
+     		<p class="error">Sign up Failed</p>
+     	<?php } ?>
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
+       
+
+          <label>User Name</label>
+          <?php if (isset($_GET['uname'])) { ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="User Name"
+                      value="<?php echo $_GET['uname']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="User Name"><br>
+          <?php }?>
+          
+          <p style="color:#DC143C;"><?php echo $er1?></p>
+
+          <label>PhoneNumber</label>
+     	<input type="text" 
+                 name="phone" 
+                 placeholder="Phone Number"><br>
+          
+          <p style="color:#DC143C;"><?php echo $er2?></p>
+          
+     
+
+     	<label>Password</label>
+     	<input type="text" 
+                 name="password" 
+                 placeholder="Password"><br>
+          <p style="color:#DC143C;"><?php echo $er3?></p>
+
+          <label>Confirm Password</label>
+          <input type="text" 
+                 name="re_password" 
+                 placeholder="Password"><br>
+          <p style="color:#DC143C;"><?php echo $er4?></p>
+
+       
+          <button type="submit">Sign Up</button>
+     </form>
+</body>
+</html>
