ce9a6c4df4bf333c7d5bf859604d07f2e5659899
Yz-Hu4Ng <52277996+Yz-Hu4Ng@users.noreply.github.com>
2021-05-12 22:55:22 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -14,23 +14,26 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 
-	if (empty($uname)) {
+	$_uname = mysql_real_escape_string($uname);
+	$_pass = mysql_real_escape_string($pass);
+
+	if (empty($_uname)) {
 		header("Location: index.php?error=User Name is required");
 	    exit();
-	}else if(empty($pass)){
+	}else if(empty($_pass)){
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
 	
 
-		$sql = "select * from User where user_name='$uname'";
+		$sql = "select * from User where user_name='$_uname'";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
 
 			$pass=hash('sha256',$pass.strval($row['salt']));
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+            if ($row['user_name'] === $_uname && $row['password'] === $_pass) {
             	$_SESSION['user_name'] = $row['user_name'];
             	$userid = $_SESSION['user_id'] = $row['user_id'];
 				$_SESSION['phone']=$row['phone'];
--- shop_search.php
+++ shop_search.php
@@ -29,6 +29,8 @@ if(isset($_SESSION['user_id'])
     $searchpricea = validate($_POST['searchpricea']);//price lower bound a
     $searchpriceb = validate($_POST['searchpriceb']);//price upper bound b
     $searchamount = validate($_POST['searchamount']);//user's input to search for masks amount
+
+
     //$search_shop_i_work = validate($_POST['search_shop_i_work']);//only showing shop that user works at or showing all 
     /*
     $sql = "search Shop.shop_name , Shop.city , Shop.mask_count , Shop.mask_price from Shop natural join Manager natural join Clerk where Shop.shop_name like %$searchshopname% and city = '$searchcity' ";
@@ -40,7 +42,7 @@ if(isset($_SESSION['user_id'])
             from Shop natural join Clerk
             where Shop.shop_name like '%$searchshopname%' and city = '$searchcity' ";
 
-
+    // 
     if($searchamount == "l") {$sql .= "and mask_count >= 100 ";$sql2 .= "and mask_count >= 100 ";}
     if($searchamount == "m") {$sql .= "and mask_count < 100 and mask_count > 50 ";$sql .= "and mask_count < 100 and mask_count > 50 ";}
     if($searchamount == "s") {$sql .= "and mask_count <= 50 ";$sql2 .= "and mask_count <= 50 ";}
